5989cda30fcf9beb4232f6812a98ff1576adf2f5
Furabio <flvdeveloper@gmail.com>
2016-10-19 19:32:06 -0200

    method create and store fixed

--- app/Http/Controllers/AnimesController.php
+++ app/Http/Controllers/AnimesController.php
@@ -7,7 +7,8 @@ use App\Category;
 use Illuminate\Http\Request;
 use App\Http\Requests\AnimeRequest;
 use App\Http\Requests;
-use DB;
+use Illuminate\Support\Facades\Session;
+use Intervention\Image\Facades\Image;
 
 class AnimesController extends Controller
 {
@@ -37,12 +38,33 @@ class AnimesController extends Controller
 
     }
 
-    public function store(AnimeRequest $request)
+    public function store(AnimeRequest $requests)
     {
-        return $request->all();
+        $image = $requests->file('image');
+        $filaname = time() . '.' . $image->getClientOriginalExtension();
+        $path = public_path('images/animes/' . $filaname);
+
+        Image::make($image->getRealPath())->resize(280, 400)->save($path);
+
+        $this->animes->create([
+            'name' => $requests->name,
+            'category_id' => $requests->category,
+            'sinopse' => $requests->sinopse,
+            'genre' => $requests->genre,
+            'image' => $filaname,
+            'director' => $requests->director,
+            'studio' => $requests->studio,
+            'release' => $requests->release,
+            'status' => $requests->status,
+            'year' => $requests->year
+        ]);
+
+        Session::flash('success', "O anime " . $requests->name . " foi  criado com sucesso!");
+
+        return redirect()->route('animes.index');
     }
 
-    public function update(AnimeRequest $request, $id)
+    public function update(AnimeRequest $requests, $id)
     {
 
     }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,10 +4,28 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
+                @include('errors.success')
                 <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
+                    <div class="panel-heading">Animes</div>
                     <div class="panel-body">
-
+                        <table class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Nome</th>
+                                    <th>Categoria</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    @foreach($animes as $anime)
+                                        <td>{{ $anime->id }}</td>
+                                        <td>{{ $anime->name }}</td>
+                                        <td>{{ $anime->category_id }}</td>
+                                    @endforeach
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
