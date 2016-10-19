f81117efb53ee25cf71cca7576de425be219e170
Furabio <flvdeveloper@gmail.com>
2016-10-19 13:13:58 -0200

    Instalação do Intervention Image

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          */
         Collective\Html\HtmlServiceProvider::class,
         Cviebrock\EloquentSluggable\ServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
         //
 
         /*
@@ -233,7 +234,9 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class,
 
-    ],
+
+],
 
 ];
